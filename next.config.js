eac76e1d6e55b4d7057ef16773f088055e91f90d
Graham Allen <grahamtallen94@gmail.com>
2021-06-27 23:11:02 -0700

    styled components for test page

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa');
+const withOptimizedImages = require('next-optimized-images');
 
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = withPWA({
-  images: {
-    domains: ['images1.fanpop.com'],
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd,
-  },
-});
+module.exports = withPWA(
+  withOptimizedImages({
+    images: {
+      domains: ['images1.fanpop.com'],
+    },
+    pwa: {
+      dest: 'public',
+      disable: !isProd,
+    },
+  })
+);
